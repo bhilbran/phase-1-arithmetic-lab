590ccdb1ad9de5feec8ae4daea714038ffcfc8e3
brooke <bhilbran@asu.edu>
2023-07-30 20:06:22 -0700

    done

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 1 
+const num2 = 62
+const multiply = num1 * num2 
 
-// Write your code here
+let random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 4
+const num4 = 1
+const mod = num3 / num4
+
+let max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
